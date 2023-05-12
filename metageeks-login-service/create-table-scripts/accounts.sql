523f6bec9eab1b3b0dfc52dd327d538db216fb19
jpatil23 <131234136+jpatil23@users.noreply.github.com>
2023-05-12 18:12:53 +0530

    Update accounts.sql
    login service Script file update.

--- metageeks-login-service/create-table-scripts/accounts.sql
+++ metageeks-login-service/create-table-scripts/accounts.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE `accounts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3
