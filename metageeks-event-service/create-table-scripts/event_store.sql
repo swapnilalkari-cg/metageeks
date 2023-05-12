d1425a26e295cd1a2c8c93237377e6089bf63b88
jpatil23 <131234136+jpatil23@users.noreply.github.com>
2023-05-12 17:13:24 +0530

    Update event_store.sql
    update event_store.sql file.

--- metageeks-event-service/create-table-scripts/event_store.sql
+++ metageeks-event-service/create-table-scripts/event_store.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE `event_store` (
+  `event_id` bigint NOT NULL AUTO_INCREMENT,
+  `event_data` json DEFAULT NULL,
+  `event_sequence` varchar(255) DEFAULT NULL,
+  `event_source` varchar(255) DEFAULT NULL,
+  `event_time` datetime DEFAULT NULL,
+  `event_type` varchar(255) NOT NULL,
+  PRIMARY KEY (`event_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=728 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
