353f860d5ac3fa41a04bebd09ff8175f395dc47f
jpatil23 <131234136+jpatil23@users.noreply.github.com>
2023-05-12 15:50:34 +0530

    Update data_provider.sql
    data_provider DDL Script added.

--- metageeks-data-provider-service/create-table-scripts/data_provider.sql
+++ metageeks-data-provider-service/create-table-scripts/data_provider.sql
@@ -1,1 +1,10 @@
-
+CREATE TABLE `data_provider` (
+  `data_provider_id` bigint NOT NULL AUTO_INCREMENT,
+  `data_provider_name` varchar(100) NOT NULL,
+  `data_provider_type` varchar(100) NOT NULL,
+  `inserted_by` varchar(100) NOT NULL,
+  `inserted_on` timestamp NOT NULL,
+  `updated_by` varchar(100) DEFAULT NULL,
+  `updated_on` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`data_provider_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
